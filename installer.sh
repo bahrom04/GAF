88cfc9c541f3bfeedce8a134f171d053b3ce51d5
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2025-04-18 11:16:31 +0500

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt -y install git
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Flatpak Apps
+flatpak install flathub org.gnome.Boxes -y
+flatpak install flathub com.visualstudio.code -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub org.gnome.Chess -y 
+flatpak install flathub org.gnome.Extensions -y 
+flatpak install flathub org.gnome.dspy -y 
+flatpak install flathub org.gnome.Builder -y
+flatpak install flathub org.gnome.Weather -y
+flatpak install flathub org.telegram.desktop -y
+
+
+# software development tool
+sudo apt -y install ssh
+sudo apt -y install vim                 
+sudo apt -y install kazam				
+sudo apt -y install chromium-browser	
+sudo apt install -y \
+curl \
+git \
+vim \
+htop \
+nodejs \
+wget \
+python-is-python3
+
+# Google chrome install
+wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+
+# GNOME
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+sudo apt install gnome-shell-extensions gnome-tweaks
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.shell favorite-apps "['nautilus.desktop', 'org.gnome.Terminal.desktop', 'firefox_firefox.desktop', 'org.gnome.Settings.desktop', 'gnome-software-local-file-packagekit.desktop', 'code.desktop', 'vmware-player.desktop', 'org.gnome.DiskUtility.desktop', 'gnome-software.desktop']"
+
+
+sudo apt-get -y install ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-compose-plugin
+sudo gpasswd -a $USER docker
+newgrp docker
+# Log off and Log in
+
+
+ 
+
