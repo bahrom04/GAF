ce53af2ca697ba5020ed96a2ff078448224ca875
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2025-05-08 11:42:53 +0500

    Update unstable.sh

--- unstable.sh
+++ unstable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt install git -y
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 flatpak_apps=(
   "org.gnome.Boxes"
   "com.visualstudio.code"
