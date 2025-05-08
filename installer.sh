88473a900ed73fc35bf4604ef79389575a10629f
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2025-05-08 11:41:44 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -32,17 +32,6 @@ gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.shell favorite-apps "['nautilus.desktop', 'org.gnome.Terminal.desktop', 'firefox_firefox.desktop', 'org.gnome.Settings.desktop', 'gnome-software-local-file-packagekit.desktop', 'code.desktop', 'vmware-player.desktop', 'org.gnome.DiskUtility.desktop', 'gnome-software.desktop']"
 
 
-sudo apt-get -y install ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo apt-get -y install docker-compose-plugin
-sudo gpasswd -a $USER docker
-newgrp docker
-# Log off and Log in
-
 
  
 
